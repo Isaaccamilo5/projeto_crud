51979c864e29bcd17232f5bbac26e052c5fa73da
isaac <isaac.camilo208@gmail.com>
2025-03-10 08:04:10 -0300

    Criação da classe resposta modelo

--- Backend/produtos/src/main/java/br/com/projetocrud/servico/ProdutoServico.java
+++ Backend/produtos/src/main/java/br/com/projetocrud/servico/ProdutoServico.java
@@ -1,5 +1,18 @@
 package br.com.projetocrud.servico;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.projetocrud.produtos.modelo.ProdutoModelo;
+import br.com.projetocrud.repositorio.ProdutoRepositorio;
+
+@Service
 public class ProdutoServico {
     
+    @Autowired
+    ProdutoRepositorio pr;
+
+   public Iterable<ProdutoModelo> listar(){
+        return pr.findAll();
+    }
 }
