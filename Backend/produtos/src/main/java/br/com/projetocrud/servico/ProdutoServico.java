88b9d514ab76ed8288769c5a9e13bf472c8a5528
isaac <isaac.camilo208@gmail.com>
2025-03-12 13:22:15 -0300

    Rota para cadastrar criada

--- Backend/produtos/src/main/java/br/com/projetocrud/servico/ProdutoServico.java
+++ Backend/produtos/src/main/java/br/com/projetocrud/servico/ProdutoServico.java
@@ -1,16 +1,22 @@
 package br.com.projetocrud.servico;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.projetocrud.produtos.modelo.ProdutoModelo;
+import br.com.projetocrud.produtos.modelo.RespostaModelo;
 import br.com.projetocrud.repositorio.ProdutoRepositorio;
 
 @Service
 public class ProdutoServico {
     
     @Autowired
-    ProdutoRepositorio pr;
+   private ProdutoRepositorio pr;
+
+    @Autowired
+    private RespostaModelo rm;
 
     //Listar produtos
    public Iterable<ProdutoModelo> listar(){
@@ -18,4 +24,16 @@ public class ProdutoServico {
     }
 
     //Cadastrar produtos 
+    public ResponseEntity<?> cadastrar (ProdutoModelo pm){
+        if(pm.getNome().equals("O nome do produto é obrigatório! ")){
+            return new ResponseEntity<RespostaModelo>(rm,HttpStatus.BAD_REQUEST);
+        } else if (pm.getMarca().equals("")) {
+            rm.setResposta("O nome da marca do produto é obrigatório! ");
+            return new ResponseEntity<RespostaModelo>(rm,HttpStatus.BAD_REQUEST);
+        }else{
+            return new ResponseEntity<ProdutoModelo>(pr.save(pm),HttpStatus.CREATED);
+        }
+    }
+
+
 }
