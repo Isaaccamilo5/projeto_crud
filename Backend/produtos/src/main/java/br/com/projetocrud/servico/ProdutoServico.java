3159d833f13b92ac39d39f0d2805454f5229e7f8
isaac <isaac.camilo208@gmail.com>
2025-03-12 10:18:21 -0300

    Produto serviço

--- Backend/produtos/src/main/java/br/com/projetocrud/servico/ProdutoServico.java
+++ Backend/produtos/src/main/java/br/com/projetocrud/servico/ProdutoServico.java
@@ -12,7 +12,10 @@ public class ProdutoServico {
     @Autowired
     ProdutoRepositorio pr;
 
+    //Listar produtos
    public Iterable<ProdutoModelo> listar(){
         return pr.findAll();
     }
+
+    //Cadastrar produtos 
 }
