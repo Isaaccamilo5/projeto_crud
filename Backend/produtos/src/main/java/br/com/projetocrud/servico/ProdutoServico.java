d0c1d8c59da97d8a7ba4014765cf0d29f1bf33c5
isaac <isaac.camilo208@gmail.com>
2025-03-18 11:43:43 -0300

    Rota para remover criada

--- Backend/produtos/src/main/java/br/com/projetocrud/servico/ProdutoServico.java
+++ Backend/produtos/src/main/java/br/com/projetocrud/servico/ProdutoServico.java
@@ -23,17 +23,29 @@ public class ProdutoServico {
         return pr.findAll();
     }
 
-    //Cadastrar produtos 
-    public ResponseEntity<?> cadastrar (ProdutoModelo pm){
-        if(pm.getNome().equals("O nome do produto é obrigatório! ")){
-            return new ResponseEntity<RespostaModelo>(rm,HttpStatus.BAD_REQUEST);
-        } else if (pm.getMarca().equals("")) {
-            rm.setResposta("O nome da marca do produto é obrigatório! ");
+    public ResponseEntity <?> cadastrarAlterar(ProdutoModelo pm, String acao){
+        if(pm.getNome().equals("")){
+            rm.setResposta("O nome do produto é obrigatório!");
+            return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
+        }else if(pm.getMarca().equals("")){
+            rm.setResposta("A marca do produto é obrigatório!");
             return new ResponseEntity<RespostaModelo>(rm,HttpStatus.BAD_REQUEST);
         }else{
-            return new ResponseEntity<ProdutoModelo>(pr.save(pm),HttpStatus.CREATED);
+            if(acao.equals("cadastrar")){
+                return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.CREATED);
+            }else{
+                return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.OK);
+            }
+        
+            
         }
+        }
+    
+    public ResponseEntity <RespostaModelo> remover(Long codigo){
+        pr.deleteById(codigo);
+            rm.setResposta("O nome do produto foi removido com sucesso! ");
+            return new ResponseEntity<RespostaModelo>(rm, HttpStatus.OK);
+
     }
-
-
 }
+    
