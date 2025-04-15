ee9815f710d4109bbb34319c03775c85e194075e
isaac <isaac.camilo208@gmail.com>
2025-04-15 09:32:41 -0300

    Personalizando botões

--- Frontend/front/src/Formulario.js
+++ Frontend/front/src/Formulario.js
@@ -1,15 +1,17 @@
 function Formulario (){
         return(
+        
+            <form className="container-form">
             
-            <form>
                 <h1>Formulário</h1>
-                <input type='text' placeholder='Nome'/>
-                <input type='text' placeholder='Marca'/>
-
-                <input type='button' value='Cadastrar'/>
-                <input type='button' value='Alterar'/>
-                <input type='button' value='Remover'/>
-                <input type='button' value='Cancelar'/>
+                <input className="entrada" type='text' placeholder='Nome'/>
+                <input className="entrada" type='text' placeholder='Marca'/>
+                <div className="contanier-btn">
+                <input className="btn btn-success" type='button' value='Cadastrar'/>
+                <input className="btn btn-primary"type='button' value='Alterar'/>
+                <input className="btn btn-danger" type='button' value='Remover'/>
+                <input className="btn btn-warning" type='button' value='Cancelar'/>
+                </div>
             </form>
             
             
