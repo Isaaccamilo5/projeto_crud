5fd9166981242a928ef58b249f3a373bcc1b803d
isaac <isaac.camilo208@gmail.com>
2025-04-29 08:02:00 -0300

    Front conectado ao back

--- Frontend/front/src/App.js
+++ Frontend/front/src/App.js
@@ -1,16 +1,25 @@
 import logo from './logo.svg';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Formulario from './Formulario';
 import Tabela from './Tabela';
 
 function App() {
 
-const[btnCadastrar] =useState(false);
+const[btnCadastrar] =useState(true);
+const[produtos, setProdutos] =useState([])
+
+ useEffect(() => {
+  fetch("http://localhost:8080/listar")
+  .then(retorno => retorno.json())
+  .then(retorno_convertido => setProdutos(retorno_convertido));
+ },[]); 
 
   return (
     <div>
+      <p>{JSON.stringify(produtos)}</p>
      <Formulario botao={btnCadastrar}/>
+     
      <Tabela/>
     </div>
   );
