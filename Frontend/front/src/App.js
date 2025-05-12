a97c07e4f17e7f612d946e9711ec3fa14e674dd7
isaac <isaac.camilo208@gmail.com>
2025-05-12 09:48:35 -0300

    Funcionalidade do botão cadastrar

--- Frontend/front/src/App.js
+++ Frontend/front/src/App.js
@@ -1,28 +1,57 @@
-
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Formulario from './Formulario';
 import Tabela from './Tabela';
 
-
-
 function App() {
 
-const[btnCadastrar] =useState(true);
-const[produtos, setProdutos] =useState([])
+  const [btnCadastrar, setBtnCadastrar] = useState(true);
+  const [produtos, setProdutos] = useState([]);
+  const [objProduto, setObjProduto] = useState({ nome: '', marca: '' });
 
- useEffect(() => {
-  fetch("http://localhost:8080/listar")
-  .then(retorno => retorno.json())
-  .then(retorno_convertido => setProdutos(retorno_convertido));
- },[]); 
+  useEffect(() => {
+    fetch("http://localhost:8080/listar")
+      .then(retorno => retorno.json())
+      .then(dados => setProdutos(dados));
+  }, []);
+
+  const aoDigitar = (e) => {
+    setObjProduto({ ...objProduto, [e.target.name]: e.target.value });
+  };
+
+  const cadastrar = () => {
+    fetch("http://localhost:8080/cadastrar", {
+      method: "POST",
+      body: JSON.stringify(objProduto),
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      }
+    })
+      .then(res => {
+        if (!res.ok) {
+          return res.json().then(err => { throw new Error(err.resposta); });
+        }
+        return res.json();
+      })
+      .then(novoProduto => {
+        alert("Produto cadastrado com sucesso!");
+        setProdutos([...produtos, novoProduto]);
+        setObjProduto({ nome: '', marca: '' });
+      })
+      .catch(erro => alert("Erro: " + erro.message));
+  };
 
   return (
-    <div>
-     <Formulario botao={btnCadastrar}/>
-     <Tabela vetor={produtos}/>  
+    <div className="container mt-3">
+      <Formulario
+        botao={btnCadastrar}
+        eventoTeclado={aoDigitar}
+        cadastrar={cadastrar}
+        obj={objProduto}
+      />
+      <Tabela vetor={produtos} />
     </div>
-
   );
 }
 
--- Frontend/front/src/Formulario.js
+++ Frontend/front/src/Formulario.js
@@ -1,22 +1,40 @@
-function Formulario({botao}){
-    return(
-        <form>
-            <h2>Sistema Gestor de Produtos</h2>
-            <input className="form-control" type="text" placeholder="Nome"></input>
-            <input className="form-control" type="text" placeholder="Marca"></input>
-            {
-               botao
-                ?
-                <input className="btn btn-success" type="button" value="Cadastrar" />
-                :
-                <div>
-                    <input className="btn btn-warning" type="button" value="Cancelar" />
-                    <input className="btn btn-primary" type="button" value="Alterar" />
-                    <input className="btn btn-danger"  type="button" value="Remover" />
-                </div>
-            }
-        </form>
-    )
+function Formulario({ botao, eventoTeclado, cadastrar, obj }) {
+  return (
+    <form>
+      <h2>Sistema Gestor de Produtos</h2>
+      <input
+        className="form-control mb-2"
+        type="text"
+        placeholder="Nome"
+        name="nome"
+        value={obj.nome}
+        onChange={eventoTeclado}
+      />
+      <input
+        className="form-control mb-2"
+        type="text"
+        placeholder="Marca"
+        name="marca"
+        value={obj.marca}
+        onChange={eventoTeclado}
+      />
+      {
+        botao ?
+          <input
+            className="btn btn-success"
+            type="button"
+            value="Cadastrar"
+            onClick={cadastrar}
+          />
+          :
+          <div>
+            <input className="btn btn-warning" type="button" value="Cancelar" />
+            <input className="btn btn-primary" type="button" value="Alterar" />
+            <input className="btn btn-danger" type="button" value="Remover" />
+          </div>
+      }
+    </form>
+  );
 }
 
 export default Formulario;
